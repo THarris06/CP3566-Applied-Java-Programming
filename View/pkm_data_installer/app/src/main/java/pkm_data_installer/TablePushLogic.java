8a7d2dba8dbb06dbadf05dc90e1e36f7a9a14085
THarris06 <144939694+THarris06@users.noreply.github.com>
2026-02-16 11:28:32 -0330

    zsadas

--- View/pkm_data_installer/app/src/main/java/pkm_data_installer/TablePushLogic.java
+++
@@ -1,9 +0,0 @@
-package pkm_data_installer;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Map;
-
-public interface TablePushLogic {
-    void configure(PreparedStatement stmt, Map<String, Object> dataSource) throws SQLException;
-}
