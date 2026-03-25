6edd2b95656947591ba9b13d0d6700f703d64e04
THarris06 <144939694+THarris06@users.noreply.github.com>
2026-03-25 10:39:52 -0230

    com

--- project/New folder/DataSource-Example/app/src/main/java/datasource/example/Messages.java
+++ project/New folder/DataSource-Example/app/src/main/java/datasource/example/Messages.java
@@ -4,47 +4,44 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.naming.Context;
 import javax.sql.DataSource;
+import javax.naming.Context;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
-@Named
+@Named("chatLogBean")
 @RequestScoped
 public class Messages {
     private List<String> chatLog = new LinkedList<>();
     private Connection conn;
-    private String message;
+    private String messageToPost;
 
-
-    public String getMessage() {
-        return message;
+    public String getMessageToPost() {
+        return messageToPost;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setMessageToPost(String messageToPost) {
+        this.messageToPost = messageToPost;
     }
 
     @PostConstruct
-    public void openConnection() {
+    public void openConnection(){
         try {
             Context ctx = new InitialContext();
             DataSource ds = (DataSource)ctx.lookup("java:/comp/env/jdbc/DsExampleDB");
-
             this.conn = ds.getConnection();
-
         } catch (NamingException | SQLException e) {
-            System.err.println(e.getMessage());
+            System.out.println(e.getMessage());
         }
+        System.out.println("PostConstruct Finished Successfully");
     }
 
     public void pullLog() {
@@ -55,48 +52,34 @@ public class Messages {
             chatLog.clear();
             while (rs.next()) {
                 StringBuilder sb = new StringBuilder();
-
                 sb.append(rs.getTimestamp(2));
                 sb.append(": ");
                 sb.append(rs.getString(1));
-
                 chatLog.add(sb.toString());
             }
         } catch (SQLException e) {
-            System.err.println(e.getMessage());
+            System.out.println(e.getMessage());
         }
+        System.out.println("PullLog Finished Successfully");
     }
 
-    public void postMessage() {
-        try (
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO messages VALUES (?, ?)")
-        ) {
-            chatLog.clear();
-
-            stmt.setString(1, message);
-            stmt.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
-            stmt.executeQuery();
-
-            this.pullLog();
-
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
+    public void postMessage(){
+        // TODO: Post a message to the DB, and update the chat log
     }
 
-    public List<String> getChatLog() {
+    public List<String> getChatLog(){
         return chatLog;
     }
 
     @PreDestroy
-    public void closeConnection() {
+    public void closeConnection(){
         if (this.conn != null) {
             try {
                 this.conn.close();
             } catch (SQLException e) {
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
         }
+        System.out.println("PreDestroy Finished Successfully");
     }
-
 }
